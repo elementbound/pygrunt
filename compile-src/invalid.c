4cc26069459dd660d55bd340bb7ea8d81797b52d
Gálffy Tamás <ezittgtx@gmail.com>
2017-03-22 22:34:36 +0200

    GCC can now find the executable automagically

---
+++ compile-src/invalid.c
@@ -0,0 +1,8 @@
+int main(int argc, char** argv) {
+    else
+        printf("Hello, %s!\n", argv[1]);
+    if(argc > 1)
+        printf("Hello, world!\n");
+
+    return 0;
+}
--- pygrunt/compiler.py
+++ pygrunt/compiler.py
@@ -8,9 +8,29 @@ class Compiler:
     def compile_project(self, project):
         pass
 
+class CompilerNotFoundException(Exception):
+    pass
+
 class GCCCompiler(Compiler):
-    def __init__(self, executable_path):
+    def __init__(self, executable_path=None):
+        import os
+
         self.executable_path = executable_path
+        if self.executable_path is None:
+            # Look for GCC in path
+            path = os.environ['PATH']
+            path = path.split(';')
+
+            print('Looking for GCC on PATH... ')
+            for dir in path:
+                if os.path.isfile(dir+'/gcc.exe'):
+                    self.executable_path = dir+'/gcc.exe'
+                    print('Found GCC at', dir)
+                    break
+
+        # Check if the executable exists
+        if not os.path.isfile(self.executable_path):
+            raise CompilerNotFoundException()
 
     def compile_file(self, in_file, out_file):
         import subprocess
@@ -19,7 +39,7 @@ class GCCCompiler(Compiler):
         args = [self.executable_path, in_file, '-o', out_file]
         result = subprocess.run(args)
 
-        if result.returncode == 0: 
+        if result.returncode == 0:
             print('success')
             return True
         else:
