ea8320cc674523528eabc18719dd38ab8ef35e01
Woster <ocsi0520@gmail.com>
2017-03-23 21:58:51 +0200

    Small adjustments to work on Ubuntu

--- project-test.py
+++ project-test.py
@@ -1,13 +1,17 @@
 import pygrunt
+import platform
 
 def main():
     project = pygrunt.Project('vecmath')
-    project.working_dir = 'compile-src/'
+    project.working_dir = 'compile-src/vecmath/'
     project.output_dir = 'build/'
 
     project.define('DEBUG')
     project.flag('Wall')
-    project.add_sources('compile-src/vecmath/*.c')
+    project.add_sources('*.c')
+
+    if platform.system() is not 'Windows':
+        project.link('m')
 
     cc = pygrunt.GCCCompiler()
     cc.optimize('size')
